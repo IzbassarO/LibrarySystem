60eabf5995c5735073eb8a09f82c6139a9d3eb0d
Izbassar <izok2004@gmail.com>
2023-02-13 00:56:32 +0600

    Changed registration parameter where it prints User with this id already exists which make id in database unique for each individual. Assylbek

--- src/LibrarySystem/entities/user/userMethods/UserController.java
+++ src/LibrarySystem/entities/user/userMethods/UserController.java
@@ -6,17 +6,23 @@ import java.util.List;
 
 public class UserController {
     private final UserRepositoryInter repo;
-
     public UserController(UserRepositoryInter repo) {
         this.repo = repo;
     }
 
     public String createUser(int id, String name, String group, String borrowedBooks) {
-        User user = new User(id, name, group, borrowedBooks);
 
-        boolean created = repo.createUser(user);
+        User user = repo.getUser(id);
 
-        return (created ? "User was created!" : "User creation was failed!");
+        if (user != null) {
+            return "User with this id already exists";
+        } else {
+            user = new User(id, name, group, borrowedBooks);
+
+            boolean created = repo.createUser(user);
+
+            return (created ? "User was created!" : "User creation was failed!");
+        }
     }
 
     public String getUser(int id) {
